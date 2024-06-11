df875021e6ce67075d8d7ae867f776691fa6afac
aldaird1 <141883146+aldaird1@users.noreply.github.com>
2024-06-11 06:05:21 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+function validarFormulario() {
+  const nombre = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const asunto = document.getElementById("asunto").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  if (nombre === "" || email === "" || asunto === "" || mensaje === "") {
+      alert("Por favor, completa todos los campos.");
+      return false;
+  }
+
+  const envioExitoso = true; // Cambia a false si el envío falla
+
+  if (envioExitoso) {
+      alert("Su fortmulario ha sido enviado, EXCELENTE!!!");
+  }
+
+  return envioExitoso; // Retorna el estado de envioExitoso
+}
